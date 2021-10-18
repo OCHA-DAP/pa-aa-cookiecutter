998f3f31f52c7acedc226425bb57ec9545b8a2ee
hannahker <hannah.ker96@gmail.com>
2021-10-18 09:57:31 -0400

    Fix typo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,6 +7,6 @@ def remove(filepath):
 
 
 if "{{cookiecutter.notebooks_in_analysis}}" == "n":
-    remove("analysis/01_anlysis_step_a.md")
+    remove("analysis/01_analysis_step_a.md")
     remove("analysis/02_analysis_step_b.md")
 
