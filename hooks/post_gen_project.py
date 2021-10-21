0fe22c1f546ca26dac3765bcf6928ec308c982dc
hannahker <hannah.ker96@gmail.com>
2021-10-21 10:20:10 -0400

    Add ma logo to docs and remove .deleteme

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,3 +9,6 @@ def remove(filepath):
 if "{{cookiecutter.notebooks_in_analysis}}" == "n":
     remove("analysis/01_analysis_step_a.md")
     remove("analysis/02_analysis_step_b.md")
+
+elif "{{cookiecutter.notebooks_in_analysis}}" == "y":
+    remove("analysis/.deleteme")
